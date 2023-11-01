8d26a56ef33d872d7f74dd2f09701a7d9cd6f242
esmaeil1378 <esmaeilrezaei13781222@gmail.com>
2023-11-01 09:14:47 +0330

    edit by yavar

--- github2/Program.cs
+++ github2/Program.cs
@@ -1,31 +1,41 @@
-﻿class program
+﻿int a=int.Parse(Console.ReadLine());
+string s = a switch
 {
+    1=>"one",
+    2=>"two",
+    3=>"three",
+    _=>"error"
+};
+Console.WriteLine($"number is :{s}");
 
-    static void Main()
-    {
+//class program
+//{
 
-        Console.WriteLine("please enter a number :");
-        int num;
-        num = int.Parse(Console.ReadLine());
-        switch (num)
-        {
-            case 1:
-                Console.WriteLine("one 1");
-                break;
-            case 2:
-                Console.WriteLine("two 2");
-                break;
-            case 3:
-                Console.WriteLine("three 3");
-                break;
-            default:
-                Console.WriteLine("error");
-                break;
-        }
+//    static void Main()
+//    {
+
+//        Console.WriteLine("please enter a number :");
+//        int num;
+//        num = int.Parse(Console.ReadLine());
+//        switch (num)
+//        {
+//            case 1:
+//                Console.WriteLine("one 1");
+//                break;
+//            case 2:
+//                Console.WriteLine("two 2");
+//                break;
+//            case 3:
+//                Console.WriteLine("three 3");
+//                break;
+//            default:
+//                Console.WriteLine("error");
+//                break;
+//        }
 
 
 
 
-    }
+//    }
 
-}
+//}
