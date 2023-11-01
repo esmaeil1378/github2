404dc05edb1da659881b681b02bdec274d65cda1
esmaeil1378 <esmaeilrezaei13781222@gmail.com>
2023-11-01 09:00:46 +0330

    Add project files.

---
+++ github2/Program.cs
@@ -0,0 +1,33 @@
+﻿class program
+{
+
+    static void Main()
+    {
+
+        Console.WriteLine("please enter a number :");
+        int num;
+        num = int.Parse(Console.ReadLine());
+        switch (num)
+        {
+            case 1:
+                Console.WriteLine("one 1");
+                break;
+            case 2:
+                Console.WriteLine("two 2");
+                break;
+            case 3:
+                Console.WriteLine("three 3");
+                break;
+            default:
+                Console.WriteLine("error");
+                break;
+        }
+
+
+        Console.WriteLine("iguvuyuw");
+
+
+
+    }
+
+}
